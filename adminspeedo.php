c00fd56b3a1a6b259fdc11e732e3d96eef9e112e
Rajin Sharwar <rajinsharwar@gmail.com>
2022-12-10 20:54:36 +0600

    Adding version 2.4.0
    Added new feature for better feedback collection.

--- adminspeedo.php
+++ adminspeedo.php
@@ -4,7 +4,7 @@
  * Description: Speed-up your slow WordPress Admin Dashboard with one click. Turn off most of the unnecessary opions for your admin panel.
  * Author: Rajin Sharwar
  * Author URI: https://linkedin.com/in/rajinsharwar
- * Version: 2.3.0
+ * Version: 2.4.0
  * Text Domain: adminsp
  */
 
@@ -18,6 +18,41 @@ function adminsp_activation_redirect( $plugin ) {
     }
 }
 add_action( 'activated_plugin', 'adminsp_activation_redirect' );
+
+if ( ! function_exists( 'adminsp_' ) ) {
+    // Create a helper function for easy SDK access.
+    function adminsp_() {
+        global $adminsp_;
+
+        if ( ! isset( $adminsp_ ) ) {
+            // Include Freemius SDK.
+            require_once dirname(__FILE__) . '/inc/vendor/start.php';
+
+            $adminsp_ = fs_dynamic_init( array(
+                'id'                  => '11603',
+                'slug'                => 'admin-speedo',
+                'type'                => 'plugin',
+                'public_key'          => 'pk_275596ba75a97a8bcbcd625edfb11',
+                'is_premium'          => false,
+                'has_addons'          => false,
+                'has_paid_plans'      => false,
+                'menu'                => array(
+                    'slug'           => 'admin-speedo',
+                    'first-path'     => 'admin.php?page=admin_speedo',
+                    'account'        => false,
+                    'support'        => false,
+                ),
+            ) );
+        }
+
+        return $adminsp_;
+    }
+
+    // Init Freemius.
+    adminsp_();
+    // Signal that SDK was initiated.
+    do_action( 'adminsp__loaded' );
+}
 
 require_once (plugin_dir_path(__FILE__). 'inc/validate.php');
 require_once (plugin_dir_path(__FILE__). 'admin/admin-menu.php');
